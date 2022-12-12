c7d895b18bd15a856dcc04a1afdccdc5dc3d4716
adnanzeki <adnanzekikarayol@gmail.com>
2022-12-12 23:21:33 +0300

    flask3 and flask4 added

--- flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
+++ flask/flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -1,14 +1,20 @@
-# bu uygulamada template kullancağımız için render_template modülünü de import ediyoruz
 from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route('/')
+
+
+
+@app.route("/")
 def head():
-    return render_template('index.html', number1 = 117000, number2 = 229000)
-    # index.html isimli templatei kullan; number1 ve number2 ise index.html argumaları
-    # index.html templates klasörü içerisinde
-@app.route('/sum')
+    return render_template("index.html", number1=34, number2=45)
+
+@app.route("/serdar")
 def number():
-    var1, var2 = 15210, 38960
-    return render_template('body.html', value1 = var1, value2 = var2, sum = var1 + var2)
-if __name__ == '__main__':
-    app.run(debug=True) # default port = 5000 fakat çalışmıyorsa boş port ataması yapılacak.
+    num1 = 23
+    num2 = 54
+    return render_template("body.html", value1=num1, value2=num2, sum=num1+num2)
+
+
+
+if __name__== "__main__":
+    app.run(debug=True)
